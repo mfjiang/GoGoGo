f2bab85fadff8500ad93889f2fd22bdd62d41bdd
江名峰 <hamlet.jiang@live.com>
2019-06-17 07:37:18 +0800

    add sql scripts

---
+++ Sql/AgileProject.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE `AgileProject` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `creater_id` bigint(20) NOT NULL,
+  `level` int(3) DEFAULT '0',
+  `state` int(3) DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `finish_date` datetime DEFAULT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
---
+++ Sql/Landmark.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE `Landmark` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `project_id` bigint(20) unsigned NOT NULL,
+  `deadline` datetime DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `remark` text,
+  `state` int(3) DEFAULT '0',
+  `finish_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
