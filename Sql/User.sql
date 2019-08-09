1b32f7edc7c1933f2c780c58e66d3c64f334e454
James <hamlet.jiang@live.com>
2019-08-09 15:01:50 +0800

    code update

---
+++ Sql/User.sql
@@ -0,0 +1,45 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : local
+Source Server Version : 80013
+Source Host           : localhost:3306
+Source Database       : gogogo
+
+Target Server Type    : MYSQL
+Target Server Version : 80013
+File Encoding         : 65001
+
+Date: 2019-08-09 14:53:30
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `wx_uuid` varchar(225) DEFAULT NULL,
+  `wx_name` varchar(255) DEFAULT NULL,
+  `nick_name` varchar(255) NOT NULL,
+  `real_name` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `pwd` varchar(255) NOT NULL,
+  `group` varchar(255) DEFAULT NULL,
+  `employee_no` varchar(255) DEFAULT NULL,
+  `mobile_no` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `is_banned` bit(1) DEFAULT b'0',
+  `last_login_date` datetime DEFAULT NULL,
+  `last_session_id` varchar(255) DEFAULT NULL,
+  `last_login_ip` varchar(255) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', null, null, 'SystemAdmin', 'James', 'System Administrator', '123456', 'admin', '1000', '13600000000', 'test@gmail.com', '\0', null, null, null, '2019-08-09 14:36:02');
