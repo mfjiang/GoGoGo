ea20192ceeb1a72fe9cbf52f4aee7550764a5fd1
Passingwind <jxnkwlp@qq.com>
2019-08-19 13:42:12 +0800

    clear temp file and update git ignore file

--- GoGoGo.WebApp/Startup.cs
+++ GoGoGo.WebApp/Startup.cs
@@ -12,30 +12,49 @@ using Microsoft.Extensions.Options;
 
 namespace GoGoGo.WebApp
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc()
+				.SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			services.AddSpaStaticFiles(config => config.RootPath = "ClientApp/dist");
+		}
 
-            app.UseMvc();
-        }
-    }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseStaticFiles();
+
+			app.UseSpaStaticFiles();
+
+			app.UseMvcWithDefaultRoute();
+
+			app.UseSpa(b =>
+			{
+				if (env.IsDevelopment())
+				{
+#if DEBUG
+					// 注释下面这行，如果没有启动前端
+					b.UseProxyToSpaDevelopmentServer("http://localhost:8080");
+#endif
+				}
+			});
+
+		}
+	}
 }
